ec5ccf999ebd8ef3447694f20a8b7b41dc66b562
Andreas Maus <maus@badphish.ypbind.de>
2022-04-23 19:13:23 +0200

    Export FlexLM expiration times

--- src/flexlm.rs
+++ src/flexlm.rs
@@ -2,12 +2,14 @@ use crate::config;
 use crate::exporter;
 use crate::license;
 
+use chrono::NaiveDateTime;
 use lazy_static::lazy_static;
 use log::{debug, error};
-use prometheus::{IntGaugeVec, Opts};
+use prometheus::{GaugeVec, IntGaugeVec, Opts};
 use regex::Regex;
 use simple_error::bail;
 use std::collections::HashMap;
+use std::env;
 use std::error::Error;
 use std::process::Command;
 
@@ -40,6 +42,22 @@ lazy_static! {
         &["app", "name", "version"],
     )
     .unwrap();
+    pub static ref FLEXLM_FEATURE_EXPIRATION: GaugeVec = GaugeVec::new(
+        Opts::new(
+            "flexlm_feature_expiration_seconds",
+            "Time until license features will expire"
+        ),
+        &["app", "index", "licenses", "name", "vendor", "version"]
+    )
+    .unwrap();
+}
+
+pub struct LicenseExpiration {
+    pub feature: String,
+    pub version: String,
+    pub license_count: i64,
+    pub expiration: f64,
+    pub vendor: String,
 }
 
 pub fn fetch(lic: &config::FlexLM, lmutil: &str) -> Result<(), Box<dyn Error>> {
@@ -58,7 +76,9 @@ pub fn fetch(lic: &config::FlexLM, lmutil: &str) -> Result<(), Box<dyn Error>> {
     let mut server_status: HashMap<String, i64> = HashMap::new();
     let mut server_master: HashMap<String, bool> = HashMap::new();
     let mut server_version: HashMap<String, String> = HashMap::new();
+    let mut license_server = String::new();
 
+    env::set_var("LANG", "C");
     debug!("flexlm.rs:fetch: Running {} -c {} -a", lmutil, &lic.license);
     let cmd = Command::new(lmutil)
         .arg("lmstat")
@@ -143,7 +163,7 @@ pub fn fetch(lic: &config::FlexLM, lmutil: &str) -> Result<(), Box<dyn Error>> {
             if capt.len() != 3 {
                 error!(
                     "Regular expression returns {} capture groups instead of 3",
-                    capt.len()
+                    capt.len(),
                 );
                 continue;
             }
@@ -159,10 +179,10 @@ pub fn fetch(lic: &config::FlexLM, lmutil: &str) -> Result<(), Box<dyn Error>> {
                 .or_insert_with(HashMap::<String, i64>::new);
             *usr.entry(version.to_string()).or_insert(0) += 1;
         } else if let Some(capt) = RE_LMSTAT_USERS_MULTI_LICENSE.captures(line) {
-            if capt.len() != 3 {
+            if capt.len() != 4 {
                 error!(
                     "Regular expression returns {} capture groups instead of 3",
-                    capt.len()
+                    capt.len(),
                 );
                 continue;
             }
@@ -194,6 +214,8 @@ pub fn fetch(lic: &config::FlexLM, lmutil: &str) -> Result<(), Box<dyn Error>> {
                 continue;
             }
             let status_line = capt.get(1).map_or("", |m| m.as_str());
+            license_server = status_line.to_string();
+
             for server_line in status_line.split(',') {
                 let srv_port: Vec<&str> = server_line.split('@').collect();
                 server_port.insert(srv_port[1].to_string(), srv_port[0].to_string());
@@ -246,6 +268,13 @@ pub fn fetch(lic: &config::FlexLM, lmutil: &str) -> Result<(), Box<dyn Error>> {
         }
     }
 
+    match fetch_expiration(lic, lmutil, license_server) {
+        Ok(_) => {}
+        Err(e) => {
+            error!("Unable to fetch expiration dates: {}", e);
+        }
+    };
+
     for server in server_status.keys() {
         let status = server_status.get(server).unwrap_or(&0);
         let _master = server_master.get(server).unwrap_or(&false);
@@ -286,6 +315,174 @@ pub fn fetch(lic: &config::FlexLM, lmutil: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn fetch_expiration(
+    lic: &config::FlexLM,
+    lmutil: &str,
+    license_server: String,
+) -> Result<(), Box<dyn Error>> {
+    lazy_static! {
+        static ref RE_LMSTAT_EXPIRATION: Regex = Regex::new(r"^([\w\-+]+)\s+([\d.]+)\s+(\d+)\s+([\w-]+)\s+(\w+)$").unwrap();
+        // Some license servers, especially on MICROS~1 Windows, report Feature,Version,#licenses,Vendor.Expires instead of Feature,Version,#licenses,Expires,Vendor
+        static ref RE_LMSTAT_ALTERNATIVE_EXPIRATION: Regex = Regex::new(r"^([\w\-+]+)\s+([\d.]+)\s+(\d+)\s+(\w+)\s+([\w-]+)$").unwrap();
+    }
+
+    let mut expiring = Vec::<LicenseExpiration>::new();
+
+    // NOTE: lmutil lmstat -i queries the  local license file. To avoid stale data, we query the extracted
+    //       license servers from  lmstat -c ... -a output instead.
+    env::set_var("LANG", "C");
+    debug!(
+        "flexlm.rs:fetch: Running {} -c {} -i",
+        lmutil, license_server
+    );
+    let cmd = Command::new(lmutil)
+        .arg("lmstat")
+        .arg("-c")
+        .arg(license_server)
+        .arg("-i")
+        .output()?;
+
+    let rc = match cmd.status.code() {
+        Some(v) => v,
+        None => {
+            bail!("Can't get return code of {} command", lmutil);
+        }
+    };
+    debug!(
+        "flexlm.rs:fetch: external command finished with exit code {}",
+        rc
+    );
+
+    if !cmd.status.success() {
+        bail!("{} command exited with non-normal exit code {}", lmutil, rc);
+    }
+
+    let stdout = String::from_utf8(cmd.stdout)?;
+    for line in stdout.lines() {
+        if let Some(capt) = RE_LMSTAT_EXPIRATION.captures(line) {
+            if capt.len() != 6 {
+                error!(
+                    "Regular expression returns {} capture groups instead of 6",
+                    capt.len()
+                );
+                continue;
+            }
+            let feature = capt.get(1).map_or("", |m| m.as_str());
+            let version = capt.get(2).map_or("", |m| m.as_str());
+            let _count = capt.get(3).map_or("", |m| m.as_str());
+            let count: i64 = match _count.parse() {
+                Ok(v) => v,
+                Err(e) => {
+                    error!("Can't parse {} as interger: {}", _count, e);
+                    continue;
+                }
+            };
+
+            let _expiration = capt.get(4).map_or("", |m| m.as_str());
+            let expiration: f64;
+            if _expiration == "1-jan-0"
+                || _expiration == "01-jan-0000"
+                || _expiration.starts_with("permanent")
+            {
+                expiration = f64::INFINITY;
+            } else {
+                expiration = match NaiveDateTime::parse_from_str(
+                    &format!("{} 00:00:00", _expiration),
+                    "%d-%b-%Y %H:%M:%S",
+                ) {
+                    Ok(v) => v.timestamp() as f64,
+                    Err(e) => {
+                        error!("Can't parse {} as date and time: {}", _expiration, e);
+                        continue;
+                    }
+                };
+            }
+
+            let vendor = capt.get(5).map_or("", |m| m.as_str());
+            expiring.push(LicenseExpiration {
+                feature: feature.to_string(),
+                version: version.to_string(),
+                license_count: count,
+                expiration: expiration,
+                vendor: vendor.to_string(),
+            });
+        } else if let Some(capt) = RE_LMSTAT_ALTERNATIVE_EXPIRATION.captures(line) {
+            if capt.len() != 6 {
+                error!(
+                    "Regular expression returns {} capture groups instead of 6",
+                    capt.len()
+                );
+                continue;
+            }
+            let feature = capt.get(1).map_or("", |m| m.as_str());
+            let version = capt.get(2).map_or("", |m| m.as_str());
+            let _count = capt.get(3).map_or("", |m| m.as_str());
+            let count: i64 = match _count.parse() {
+                Ok(v) => v,
+                Err(e) => {
+                    error!("Can't parse {} as interger: {}", _count, e);
+                    continue;
+                }
+            };
+
+            let _expiration = capt.get(5).map_or("", |m| m.as_str());
+            let expiration: f64;
+            if _expiration == "1-jan-0"
+                || _expiration == "01-jan-0000"
+                || _expiration.starts_with("permanent")
+            {
+                expiration = f64::INFINITY;
+            } else {
+                expiration = match NaiveDateTime::parse_from_str(
+                    &format!("{} 00:00:00", _expiration),
+                    "%d-%b-%Y %H:%M:%S",
+                ) {
+                    Ok(v) => v.timestamp() as f64,
+                    Err(e) => {
+                        error!("Can't parse {} as date and time: {}", _expiration, e);
+                        continue;
+                    }
+                };
+            }
+
+            let vendor = capt.get(4).map_or("", |m| m.as_str());
+            expiring.push(LicenseExpiration {
+                feature: feature.to_string(),
+                version: version.to_string(),
+                license_count: count,
+                expiration: expiration,
+                vendor: vendor.to_string(),
+            });
+        }
+    }
+
+    let mut index: i64 = 1;
+    for entry in expiring {
+        debug!(
+            "flexlm.rs:fetch: Setting flexlm_feature_used_users -> {} {} {} {} {} {} {}",
+            lic.name,
+            index,
+            entry.license_count.to_string(),
+            entry.feature,
+            entry.vendor,
+            entry.version,
+            entry.expiration
+        );
+        FLEXLM_FEATURE_EXPIRATION
+            .with_label_values(&[
+                &lic.name,
+                &index.to_string(),
+                &entry.license_count.to_string(),
+                &entry.feature,
+                &entry.vendor,
+                &entry.version,
+            ])
+            .set(entry.expiration);
+        index += 1;
+    }
+    Ok(())
+}
+
 pub fn register() {
     exporter::REGISTRY
         .register(Box::new(FLEXLM_FEATURES_TOTAL.clone()))
@@ -301,5 +498,8 @@ pub fn register() {
         .unwrap();
     exporter::REGISTRY
         .register(Box::new(FLEXLM_VENDOR_STATUS.clone()))
+        .unwrap();
+    exporter::REGISTRY
+        .register(Box::new(FLEXLM_FEATURE_EXPIRATION.clone()))
         .unwrap();
 }
